65d5a874ac6d7f205100ad6b7c37e2cde2e5b3a3
Sorami Hisamoto <s@89.io>
2021-12-10 18:22:13 +0900

    Add icon and link

--- app/src/lib/Header.svelte
+++ app/src/lib/Header.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import 'material-design-icons/iconfont/material-icons.css';
 	import { flyTo } from '$lib/map/Map.svelte';
 
 	function flyToOrigin() {
@@ -7,8 +8,15 @@
 </script>
 
 <header>
-	<h1 on:click={flyToOrigin}>A People Map of Japan</h1>
-	<p><a href="https://sorami.dev/" target="_blank" rel="noreferrer">sorami.dev</a></p>
+	<div class="website">
+		<a href="https://sorami.dev/" target="_blank" rel="noreferrer">sorami.dev</a>
+	</div>
+	<div class="title" on:click={flyToOrigin}>
+		A People Map of Japan
+	</div>
+	<div class="about">
+		<i class="material-icons">help_outline</i>
+	</div>
 </header>
 
 <style>
@@ -16,43 +24,56 @@
 		z-index: 1;
 		position: absolute;
 		width: 100%;
-		margin: 0;
+		margin-bottom: 1em;
 		padding: 0.25em 0;
-		box-shadow: 0 4px 8px 0 rgb(0, 0, 0, 0.5);
-		background-color: rgba(0, 0, 0, 0.75);
-		display: flex;
-		justify-content: center;
+			
+		display: inline-flex;
+		justify-content: space-between;
 		align-items: center;
+
+		box-shadow: 0 4px 8px 0 rgb(0, 0, 0, 0.25);
+		background-color: rgba(0, 0, 0, 0.6);
 	}
 
-	h1 {
+	.website {
+		margin-left: 1em;
+		font-size: 0.8em;
+		font-weight: bold;
+	}
+	.website a {
+		color: #fff;
+		opacity: 0.25;
+		text-decoration: none;
+	}
+	.website a:hover {
 		color: #34be82;
-		font-size: 1em;
+		text-decoration: underline;
+		opacity: 1;
+	}
+
+	.title {
+		color: #34be82;
+		font-size: 1.2em;
 		font-weight: bold;
 		padding: 0;
 		margin: 0;
 		opacity: 0.8;
 	}
-	h1:hover {
+	.title:hover {
 		cursor: pointer;
 	}
 
-	p {
-		position: absolute;
-		right: 0;
-		margin: 0;
-		padding: 0;
-		margin-right: 1.5em;
-		font-size: 0.75em;
-		font-weight: 700;
-	}
-	p a {
+	.about {
+		margin-right: 1em;
+		opacity: 0.5;
 		color: #fff;
-		opacity: 0.4;
-		text-decoration: none;
 	}
-	p a:hover {
-		text-decoration: underline;
-		opacity: 0.75;
+	.about:hover {
+		cursor: pointer;
+		opacity: 1;
+		color: #34be82;
+	}
+	.about .material-icons {
+		vertical-align: middle;
 	}
 </style>
