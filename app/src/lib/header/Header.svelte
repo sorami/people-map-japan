eb0005328e104367328991debf59de7641027a26
Sorami Hisamoto <s@89.io>
2021-12-11 15:21:17 +0900

    Change header text to icon

--- app/src/lib/header/Header.svelte
+++ app/src/lib/header/Header.svelte
@@ -4,11 +4,13 @@
 </script>
 
 <header>
-	<div class="website">
-		<a href="https://sorami.dev/" target="_blank" rel="noreferrer">sorami.dev</a>
+	<div class="icon">
+		<a href="https://sorami.dev/" target="_blank" rel="noreferrer">
+			<i class="material-icons">home</i>
+		</a>
 	</div>
 	<div class="title" on:click={() => getModal().open()}>A People Map of Japan</div>
-	<div class="about" on:click={() => getModal().open()}>
+	<div class="icon" on:click={() => getModal().open()}>
 		<i class="material-icons">help_outline</i>
 	</div>
 </header>
@@ -31,23 +33,6 @@
 		box-shadow: 0 2px 4px 0 rgb(33, 33, 33, 0.25);
 	}
 
-	.website {
-		margin-left: 1em;
-		font-size: 1em;
-		font-weight: bold;
-		font-family: monospace;
-	}
-	.website a {
-		color: #fff;
-		opacity: 0.5;
-		text-decoration: none;
-	}
-	.website a:hover {
-		color: #34be82;
-		text-decoration: underline;
-		opacity: 1;
-	}
-
 	.title {
 		color: #fff;
 		font-size: 1.2em;
@@ -61,17 +46,20 @@
 		cursor: pointer;
 	}
 
-	.about {
-		margin-right: 1em;
+	.icon {
+		margin: 0 0.5em;
 		opacity: 0.75;
 		color: #fff;
 	}
-	.about:hover {
+	.icon a {
+		color: #fff;
+	}
+	.icon:hover {
 		cursor: pointer;
 		opacity: 1;
 		color: #34be82;
 	}
-	.about .material-icons {
+	.icon .material-icons {
 		vertical-align: middle;
 	}
 </style>
